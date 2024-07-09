23d633c5304265287ee78cd25dcbf61787a2583c
Yousef Hadder <yousefhadder94@gmail.com>
2024-07-09 14:30:52 -0500

    add local data & HTTP requests handling

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Natours-nodejs
 
-This porject is an implementation of the Natours project of the NodeJs bootcamp by Jonas Schmedtmann.
+This project is an implementation of the Natours project of the NodeJs bootcamp by Jonas Schmedtmann.
--- app.js
+++ app.js
@@ -1,17 +1,104 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.status(200).json({
-		message: 'Hello from the server side!',
-		app: 'Natours',
-	});
-});
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
+);
 
-app.post('/', (req, res) => {
-	res.send('You can post to this endpoint...');
-});
+app.use(express.json());
+
+const getAllTours = (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		results: tours.length,
+		data: { tours },
+	});
+};
+
+const getTourById = (req, res) => {
+	const { id } = req.params;
+	const tour = tours.find((t) => t.id === parseInt(id));
+	if (!tour) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID.',
+		});
+	}
+	res.status(200).json({
+		status: 'success',
+		data: { tour },
+	});
+};
+
+const createTour = (req, res) => {
+	const newId = tours[tours.length - 1].id + 1;
+	const newTour = Object.assign({ id: newId }, req.body);
+	tours.push(newTour);
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		(err) => {
+			res.status(201).json({
+				status: 'success',
+				message: 'New tour created.',
+				data: { tour: newTour },
+			});
+		},
+	);
+};
+const updateTourById = (req, res) => {
+	const { id } = req.params;
+	const index = tours.findIndex((t) => t.id === parseInt(id));
+	if (index === -1) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID.',
+		});
+	}
+	const updatedTour = Object.assign(tours[index], req.body);
+	tours[index] = updatedTour;
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		(err) => {
+			res.status(200).json({
+				status: 'success',
+				message: 'tour updated.',
+				data: { tour: updatedTour },
+			});
+		},
+	);
+};
+const deleteTourById = (req, res) => {
+	const { id } = req.params;
+	const index = tours.findIndex((t) => t.id === parseInt(id));
+	if (index === -1) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID.',
+		});
+	}
+	tours.splice(index, 1);
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		(err) => {
+			res.status(200).json({
+				status: 'success',
+				message: 'tour deleted.',
+			});
+		},
+	);
+};
+
+// API routes
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTourById);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTourById);
+app.delete('/api/v1/tours/:id', deleteTourById);
 
 const PORT = 3000;
 app.listen(PORT, () => {
