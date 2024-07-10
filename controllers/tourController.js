a388d1317faed9b41138dcafdf1b5ac7aa735cb2
Yousef Hadder <yousefhadder94@gmail.com>
2024-07-10 18:42:27 -0500

    Add full Tour Schema & Implement Request Query Params

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,67 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
 	try {
-		const tours = await Tour.find();
+		// 1) Build the query
+
+		// Destructuring query parameters
+		const queryObj = { ...req.query };
+
+		// Those fields are related to how the data is presented rather than
+		// the data selection criteria so we exclude them from the query
+		const excludedFields = ['page', 'sort', 'limit', 'fields'];
+		excludedFields.forEach((el) => delete queryObj[el]);
+		// 2) Advanced filtering
+		const replaceMongoOperators = (key) => `$${key}`;
+
+		// Replace operators like gt, gte, lt, lte, in with $gt, $gte, $lt, $lte, $in
+		let queryStr = JSON.stringify(queryObj).replace(
+			/\b(gt|gte|lt|lte|in)\b/g,
+			replaceMongoOperators,
+		);
+
+		// Function to handle special characters in the query string
+		const handleSpecialCharacters = (match) =>
+			match[1] === '[' ? match[1] : match[0];
+
+		// Handle special characters like quoted square brackets
+		queryStr = queryStr.replace(
+			/(['"]\[|\]['"])/g,
+			handleSpecialCharacters,
+		);
+
+		// Remove backslashes
+		queryStr = queryStr.replace(/\\/g, '');
+
+		// 2) Sorting
+		const sortBy = req.query.sort
+			? req.query.sort.split(',').join(' ')
+			: '-createdAt';
+
+		// 3) Fields Limiting
+		const fields = req.query.fields
+			? req.query.fields.split(',').join(' ')
+			: '-__v';
+
+		// 4) Pagination
+		const page = Number(req.query.page) || 1;
+		const limit = Number(req.query.limit) || 10;
+		const skip = (page - 1) * limit;
+
+		if (req.query.page) {
+			const numTours = await Tour.countDocuments();
+			if (skip >= numTours) {
+				throw new Error('Page not found');
+			}
+		}
+
+		const tourQuery = Tour.find(JSON.parse(queryStr))
+			.sort(sortBy)
+			.select(fields)
+			.skip(skip)
+			.limit(limit);
+
+		// 5) Execute the query
+		const tours = await tourQuery;
 		res.status(200).json({
 			status: 'success',
 			results: tours.length,
@@ -34,9 +94,7 @@ exports.getTourById = async (req, res) => {
 
 exports.createTour = async (req, res) => {
 	try {
-		const tour = await Tour.create(req.body, {
-			runValidators: true,
-		});
+		const tour = await Tour.create(req.body);
 		res.status(201).json({
 			status: 'success',
 			data: { tour },
