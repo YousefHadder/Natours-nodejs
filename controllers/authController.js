76756a0ec01691d28e57dede7c3af003a2e27211
Yousef Hadder <yousefhadder94@gmail.com>
2024-07-15 19:29:38 -0500

    Implement a fix for signup process, use allowed data to signup new users

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,18 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const newUser = await User.create(req.body);
+	// With the below line, a new user can send a role within the request body
+	// and assume the role on the server (like admin)
+	// const newUser = await User.create(req.body);
+
+	// Ensuring only the data we want is used to register a new user
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
+
 	res.status(201).json({
 		status: 'success',
 		data: { user: newUser },
