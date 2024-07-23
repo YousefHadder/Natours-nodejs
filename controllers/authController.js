dde79a59a5fb1f9e6d1099a110474a79a16647f3
Yousef Hadder <yousefhadder94@gmail.com>
2024-07-23 06:09:56 +0300

    implement httpOnly cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,11 +11,30 @@ const signToken = (id) =>
 		expiresIn: process.env.JWT_EXPIRES_IN,
 	});
 
-const createAndSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, res) => {
 	const token = signToken(user._id);
+	const cookieOptions = {
+		expires: new Date(
+			Date.now() +
+				process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+		),
+		httpOnly: true,
+	};
+
+	if (process.env.NODE_ENV === 'production') {
+		cookieOptions.secure = true;
+	}
+
+	res.cookie('jwt', token, cookieOptions);
+
+	user.password = undefined;
+
 	res.status(statusCode).json({
 		status: 'success',
 		token,
+		data: {
+			user,
+		},
 	});
 };
 
@@ -33,7 +52,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 		passwordConfirm: req.body.passwordConfirm,
 		passwordChangedAt: req.body.passwordChangedAt,
 	});
-	createAndSendToken(newUser, 201, res);
+	createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -54,7 +73,7 @@ exports.login = catchAsync(async (req, res, next) => {
 		return next(new AppError('Incorrect email or password', 401));
 	}
 	// 3) If everything is ok, send token to the client
-	createAndSendToken(user, 200, res);
+	createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -165,11 +184,10 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 	// 3) Update changedPasswordAt property for the user
 	// 4) Log the user in, send the JWT
-	createAndSendToken(user, 200, res);
+	createSendToken(user, 200, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
-	console.log(req.user);
 	// 1) Get user from collection
 	const user = await User.findById(req.user.id).select('+password');
 
@@ -193,5 +211,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 	// So we use save() instead
 
 	// 4) Log user in, Send JWT back
-	createAndSendToken(user, 200, res);
+	createSendToken(user, 200, res);
 });
