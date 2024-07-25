77e69d9833411bef4fbb04f8077dbabf959f7caa
Yousef Hadder <yousefhadder94@gmail.com>
2024-07-25 12:19:36 -0500

    Implement calculating review statistics

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 // review / rating / createdAt / ref to the tour / ref to the user
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = mongoose.Schema(
 	{
@@ -34,18 +35,50 @@ const reviewSchema = mongoose.Schema(
 	},
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 // Populate tour and user in the review response
 reviewSchema.pre(/^find/, async function (next) {
-	this.populate({
-		path: 'tour',
-		select: 'name',
-	});
 	this.populate({
 		path: 'user',
 		select: 'name photo',
 	});
 
 	next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+	const stats = await this.aggregate([
+		{
+			$match: { tour: tourId },
+		},
+		{
+			$group: {
+				_id: '$tour',
+				nRating: { $sum: 1 },
+				avgRating: { $avg: '$rating' },
+			},
+		},
+	]);
+	await Tour.findByIdAndUpdate(tourId, {
+		ratingsAverage: stats[0]?.avgRating || 4.5,
+		ratingsQuantity: stats[0]?.nRatings || 0,
+	});
+};
+
+reviewSchema.post('save', function () {
+	// this points to the current review document
+	this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+	this.r = await this.clone().findOne();
+	next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+	// await this.findOne(); does NOT work here, query has already executed
+	await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
